6aa6efb093335c6b9bc4c0304aa3bd4208b60c82
Roland Tritsch <roland@tritsch.org>
2019-02-15 15:28:34 +0000

    p102-p104: Initial commit.

--- lib/ninety_nine_problems.ex
+++ lib/ninety_nine_problems.ex
@@ -8,4 +8,23 @@ defmodule NinetyNineProblems do
   """
   def p101_last([e | []]), do: e
   def p101_last([_ | rest]), do: p101_last(rest)
+
+  @doc """
+  Find the last but one element of a list.
+  """
+  def p102_lastBut([e | [_]]), do: e
+  def p102_lastBut([_ | rest]), do: p102_lastBut(rest)
+
+  @doc """
+  Find the K'th element of a list.
+  """
+  def p103_elementAt([e | _], 0), do: e
+  def p103_elementAt([_ | rest], k), do: p103_elementAt(rest, k - 1)
+
+  @doc """
+  Find the number of elements of a list.
+  """
+  def p104_length(l), do: p104_lengthN(l, 0)
+  def p104_lengthN([], n), do: n
+  def p104_lengthN([_ | rest], n), do: p104_lengthN(rest, n + 1)
 end
--- test/ninety_nine_problems_test.exs
+++ test/ninety_nine_problems_test.exs
@@ -5,4 +5,16 @@ defmodule NinetyNineProblemsTest do
   test "p101_last(list)" do
     assert NinetyNineProblems.p101_last([1, 2, 99]) == 99
   end
+
+  test "p102_lastBut(list)" do
+    assert NinetyNineProblems.p102_lastBut([1, 2, 99, 0]) == 99
+  end
+
+  test "p103_elementAt(list, k)" do
+    assert NinetyNineProblems.p103_elementAt([1, 2, 99, 0], 2) == 99
+  end
+
+  test "p104_length(list)" do
+    assert NinetyNineProblems.p104_length([1, 2, 99, 0]) == 4
+  end
 end
