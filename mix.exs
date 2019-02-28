a3a0ad7721cfc72d6898daaf94bdef716da83982
Roland Tritsch <roland@tritsch.org>
2019-02-28 11:03:37 +0000

    Make 'mix docs' work/pass.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NinetyNineProblems.MixProject do
     [
       app: :ninety_nine_problems,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -22,7 +22,9 @@ defmodule NinetyNineProblems.MixProject do
   defp deps do
     [
       {:random, ">= 0.2.4"},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:earmark, "~> 1.2", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
 end
