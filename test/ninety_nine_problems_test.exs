2d1523ab5beb67bb71e4cf47e312f9fd8547dbfc
Roland Tritsch <roland@tritsch.org>
2019-03-03 11:00:03 +0000

    Adding more property based testing.

--- test/ninety_nine_problems_test.exs
+++ test/ninety_nine_problems_test.exs
@@ -107,6 +107,7 @@ defmodule NinetyNineProblemsTest do
       end
       finalize = fn acc -> {:cont, acc, []} end
       [head | tail] = l_
+
       assert p109_pack(l_) == Enum.chunk_while(tail, [head], same, finalize)
     end
   end
@@ -133,20 +134,61 @@ defmodule NinetyNineProblemsTest do
     assert p113_encode_direct(['a','a','a','a','b','c','c','a','a','d','e','e','e','e']) == [{4, 'a'}, 'b', {2, 'c'}, {2, 'a'}, 'd', {4, 'e'}]
   end
 
+  property "Duplicate must duplicate :)" do
+    check all l <- StreamData.list_of(StreamData.integer, min_length: 1) do
+      assert p114_duplicate(l) |> Enum.sort == List.duplicate(l, 2) |> List.flatten |> Enum.sort
+    end
+  end
+
   test "p114_duplicate(list)" do
     assert p114_duplicate(['a','b','c','c','d']) == ['a','a','b','b','c','c','c','c','d','d']
+  end
+
+  property "Duplicate must duplicate (n times) :)" do
+    check all l <- StreamData.list_of(StreamData.integer, min_length: 1),
+              n <- StreamData.positive_integer
+    do
+      assert p115_duplicate_n(l, n) |> Enum.sort == List.duplicate(l, n) |> List.flatten |> Enum.sort
+    end
   end
 
   test "p115_duplicate_n(list)" do
     assert p115_duplicate_n(['a','b','c'], 3) == ['a','a','a','b','b','b','c','c','c']
   end
 
+  property "Drop must drop :)" do
+    check all l <- StreamData.list_of(StreamData.integer, min_length: 3),
+              n <- StreamData.integer(2..(length(l) - 1))
+    do
+      assert p116_drop_every(List.delete_at(l, 0), n) == Enum.drop_every(l, n)
+    end
+  end
+
   test "p116_drop_every(list, n)" do
+    assert Enum.to_list(1..10) |> p116_drop_every(2) == [1,3,5,7,9]
+    assert Enum.to_list(1..10) |> Enum.drop_every(2) == [2,4,6,8,10]
     assert p116_drop_every(['a','b','c','d','e','f','g','h','i','k'], 3) == ['a','b','d','e','g','h','k']
+  end
+
+  property "Split must split :)" do
+    check all l <- StreamData.list_of(StreamData.integer),
+              n <- StreamData.integer(0..(length(l) - 1))
+    do
+      assert p117_split_at(l, n) == Enum.split(l, n)
+    end
   end
 
   test "p117_split_at(list, n)" do
     assert p117_split_at(['a','b','c','d','e','f','g','h','i','k'], 3) == {['a','b','c'], ['d','e','f','g','h','i','k']}
+  end
+
+  property "Slice must sclice :)" do
+    check all l <- StreamData.list_of(StreamData.integer),
+              i <- StreamData.integer(0..(length(l) - 1)),
+              k <- StreamData.integer((i+1)..(length(l) - 1))
+    do
+      assert p118_slice(l, i+1, k+1) == Enum.slice(l, i, k - i + 1)
+    end
   end
 
   test "p118_slice(list, i, k)" do
@@ -158,12 +200,37 @@ defmodule NinetyNineProblemsTest do
     assert p119_rotate_n(['a','b','c','d','e','f','g','h'], -2) == ['g','h','a','b','c','d','e','f']
   end
 
+  property "Remove must remove :)" do
+    check all l <- StreamData.list_of(StreamData.integer),
+              k <- StreamData.integer(0..(length(l) - 1))
+    do
+      assert p120_remove_at(l, k+1) |> elem(1) == List.delete_at(l, k)
+    end
+  end
+
   test "p120_remove_at(list, k)" do
     assert p120_remove_at(['a','b','c','d'], 2) == {'b', ['a','c','d']}
   end
 
+  property "Insert must insert :)" do
+    check all l <- StreamData.list_of(StreamData.integer),
+              k <- StreamData.integer(0..(length(l) - 1)),
+              e <- StreamData.integer
+    do
+      assert p121_insert_at(l, k+1, e) == List.insert_at(l, k, e)
+    end
+  end
+
   test "p121_insert_at(list, k, e)" do
     assert p121_insert_at(['a','b','c','d'], 2, 'z') == ['a','z','b','c','d']
+  end
+
+  property "Range is a range :)" do
+    check all from <- StreamData.integer,
+              to <- StreamData.integer
+    do
+      assert p122_range(from, to) == Enum.to_list(from..to)
+    end
   end
 
   test "p122_range(from, to)" do
