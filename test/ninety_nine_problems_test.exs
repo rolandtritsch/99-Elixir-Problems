03a9048c03851c8f47d523884b66c0765061310d
Roland Tritsch <roland@tritsch.org>
2019-03-14 19:25:25 +0000

    excheck: Done playing. Staying with streamdata. For now.

--- test/ninety_nine_problems_test.exs
+++ test/ninety_nine_problems_test.exs
@@ -1,5 +1,5 @@
 defmodule NinetyNineProblemsTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
   use ExCheck
 
   doctest NinetyNineProblems
@@ -7,106 +7,114 @@ defmodule NinetyNineProblemsTest do
   import NinetyNineProblems
 
   property :last_must_be_last do
-    for_all l in list(int()), do: assert p101_last(l) == List.last(l)
+    for_all l in list(int()) do
+      implies length(l) > 0 do
+        p101_last(l) == List.last(l)
+      end
+    end
   end
 
   test "p101_last(list)" do
     assert p101_last([1, 2, 99]) == 99
   end
-"""
-  property "But last element must always be but last :)" do
-    check all l <- StreamData.list_of(StreamData.integer(), min_length: 2) do
-      assert p102_last_but(l) == Enum.at(l, length(l) - 2)
+
+  property :last_bat_last_must_be_last do
+    for_all l in list(int()) do
+      implies length(l) > 2 do
+        p102_last_but(l) == Enum.at(l, length(l) - 2)
+      end
     end
   end
-"""
+
   test "p102_last_but(list)" do
     assert p102_last_but([1, 2, 99, 0]) == 99
   end
-"""
-  property "Element at k must always be at k :)" do
-    check all l <- StreamData.list_of(StreamData.integer(), min_length: 1),
-              k <- StreamData.positive_integer(),
-              k_ = rem(k, length(l)) do
-      assert p103_element_at(l, k_) == Enum.at(l, k_)
+
+  property :element_at_k_is_at_k do
+    for_all {l, k} in {list(int()), pos_integer()} do
+      implies length(l) > 0 do
+        k_ = rem(k, length(l))
+        p103_element_at(l, k_) == Enum.at(l, k_)
+      end
     end
   end
-"""
+
   test "p103_element_at(list, k)" do
     assert p103_element_at([1, 2, 99, 0], 2) == 99
   end
-"""
-  property "Length must always be length :)" do
-    check all l <- StreamData.list_of(StreamData.integer()) do
-      assert p104_length(l) == length(l)
+
+  property :length_must_be_length do
+    for_all l in list(int()) do
+      p104_length(l) == length(l)
     end
   end
-"""
+
   test "p104_length(list)" do
     assert p104_length([1, 2, 99, 0]) == 4
   end
-"""
-  property "Reverse a list twice is the list again" do
-    check all l <- StreamData.list_of(StreamData.integer()) do
-      assert l |> p105_reverse |> p105_reverse == l
+
+  property :reverse_reverse_returns_the_list do
+    for_all l in list(int()) do
+      l |> p105_reverse |> p105_reverse == l
     end
   end
-"""
+
   test "p105_reverse(list)" do
     assert p105_reverse([1, 2, 99, 0]) == [0, 99, 2, 1]
   end
-"""
-  property "Palindroms are palindroms :)" do
-    check all l <- StreamData.list_of(StreamData.integer()),
-              n <- StreamData.integer(),
-              p = l ++ [n] ++ Enum.reverse(l) do
-      assert p106_palindrom?(p)
+
+  property :palindroms_are_palindroms do
+    for_all {l, n} in {list(int()), int()} do
+      p = l ++ [n] ++ Enum.reverse(l)
+      p106_palindrom?(p)
     end
   end
-"""
+
   test "p106_isPalindrom(list)" do
     assert p106_palindrom?('racecar')
   end
-"""
-  property "Flatten must flatten :)" do
-    check all l <- StreamData.list_of(StreamData.integer()),
-              l_ = Enum.map(l, &[&1]) do
-      assert p107_flatten(l_) == List.flatten(l_)
+
+  property :flatten_must_flatten do
+    for_all l in list(int()) do
+      l_ = Enum.map(l, &[&1])
+      p107_flatten(l_) == List.flatten(l_)
     end
   end
-"""
+
   test "p107_flatten(list)" do
     assert p107_flatten([1, [2, 2], 1, [2, 2, [3, 3, 3]]]) == [1, 2, 2, 1, 2, 2, 3, 3, 3]
   end
-"""
-  property "Compress must dedup :)" do
-    check all l <- StreamData.list_of(StreamData.integer()),
-              l_ = Enum.sort(l) do
-      assert p108_compress(l_) == Enum.dedup(l_)
+
+  property :compress_must_dedup do
+    for_all l in list(int()) do
+      l_ = Enum.sort(l)
+      p108_compress(l_) == Enum.dedup(l_)
     end
   end
-"""
+
   test "p108_compress(list)" do
     assert p108_compress([1, 2, 2, 1, 2, 2, 3, 3, 3]) == [1, 2, 1, 2, 3]
 
     assert p108_compress('aaaabccaadeeee') == [?a, ?b, ?c, ?a, ?d, ?e]
   end
-"""
+
   property "Pack must pack :)" do
-    check all l <- StreamData.list_of(StreamData.integer(), min_length: 1),
-              l_ = Enum.sort(l) do
-      same = fn
-        e, [a | rest] when e == a -> {:cont, [e | [a | rest]]}
-        e, acc -> {:cont, acc, [e]}
+    for_all l in list(int()) do
+      implies length(l) > 0 do
+        l_ = Enum.sort(l)
+        same = fn
+          e, [a | rest] when e == a -> {:cont, [e | [a | rest]]}
+          e, acc -> {:cont, acc, [e]}
+        end
+
+        finalize = fn acc -> {:cont, acc, []} end
+        [head | tail] = l_
+
+        p109_pack(l_) == Enum.chunk_while(tail, [head], same, finalize)
       end
-
-      finalize = fn acc -> {:cont, acc, []} end
-      [head | tail] = l_
-
-      assert p109_pack(l_) == Enum.chunk_while(tail, [head], same, finalize)
     end
   end
-"""
+
   test "p109_pack(list)" do
     assert p109_pack([1, 2, 2, 1, 2, 2, 3, 3, 3]) == [[1], [2, 2], [1], [2, 2], [3, 3, 3]]
 
@@ -141,13 +149,15 @@ defmodule NinetyNineProblemsTest do
              {4, ?e}
            ]
   end
-"""
-  property "Decode an encoding must give the original string :)" do
-    check all l <- StreamData.string(?a..?z, min_length: 1) do
-      assert to_charlist(l) |> p111_encode_modified |> p112_decode == to_charlist(l)
+
+  property :decode_encode_is_the_string do
+    for_all l in list(char()) do
+      implies length(l) > 0 do
+        to_charlist(l) |> p111_encode_modified |> p112_decode == to_charlist(l)
+      end
     end
   end
-"""
+
   test "p112_decode(list)" do
     assert p112_decode([{4, 'a'}, 'b', {2, 'c'}, {2, 'a'}, 'd', {4, 'e'}]) ==
              ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e']
@@ -159,66 +169,45 @@ defmodule NinetyNineProblemsTest do
     assert p113_encode_direct('aaaabccaadeeee') ==
              [{4, ?a}, ?b, {2, ?c}, {2, ?a}, ?d, {4, ?e}]
   end
-"""
-  property "Duplicate must duplicate :)" do
-    check all l <- StreamData.list_of(StreamData.integer(), min_length: 1) do
-      assert p114_duplicate(l) |> Enum.sort() ==
-               List.duplicate(l, 2) |> List.flatten() |> Enum.sort()
+
+  property :duplicate_must_duplicate do
+    for_all l in list(int()) do
+      implies length(l) > 0 do
+        l |> p114_duplicate() |> Enum.sort() == l |> List.duplicate(2) |> List.flatten() |> Enum.sort()
+      end
     end
   end
-"""
+
   test "p114_duplicate(list)" do
     assert p114_duplicate('abccd') == [?a, ?a, ?b, ?b, ?c, ?c, ?c, ?c, ?d, ?d]
   end
-"""
-  property "Duplicate must duplicate (n times) :)" do
-    check all l <- StreamData.list_of(StreamData.integer(), min_length: 1),
-              n <- StreamData.positive_integer() do
-      assert p115_duplicate_n(l, n) |> Enum.sort() ==
-               List.duplicate(l, n) |> List.flatten() |> Enum.sort()
+
+  property :duplicate_must_duplicate_n_times do
+    for_all {l, n} in {list(int()), pos_integer()} do
+      implies length(l) > 0 do
+        l |> p115_duplicate_n(n) |> Enum.sort() == l |> List.duplicate(n) |> List.flatten() |> Enum.sort()
+      end
     end
   end
-"""
+
   test "p115_duplicate_n(list)" do
     assert p115_duplicate_n('abc', 3) == [?a, ?a, ?a, ?b, ?b, ?b, ?c, ?c, ?c]
   end
-"""
-  property "Drop must drop :)" do
-    check all l <- StreamData.list_of(StreamData.integer(), min_length: 3),
-              n <- StreamData.integer(2..(length(l) - 1)) do
-      assert p116_drop_every(List.delete_at(l, 0), n) == Enum.drop_every(l, n)
-    end
-  end
-"""
+
   test "p116_drop_every(list, n)" do
     assert Enum.to_list(1..10) |> p116_drop_every(2) == [1, 3, 5, 7, 9]
     assert Enum.to_list(1..10) |> Enum.drop_every(2) == [2, 4, 6, 8, 10]
 
     assert p116_drop_every('abcdefghik', 3) == [?a, ?b, ?d, ?e, ?g, ?h, ?k]
   end
-"""
-  property "Split must split :)" do
-    check all l <- StreamData.list_of(StreamData.integer()),
-              n <- StreamData.integer(0..(length(l) - 1)) do
-      assert p117_split_at(l, n) == Enum.split(l, n)
-    end
-  end
-"""
+
   test "p117_split_at(list, n)" do
     assert p117_split_at('abcdefghik', 3) == {
              [?a, ?b, ?c],
              [?d, ?e, ?f, ?g, ?h, ?i, ?k]
            }
   end
-"""
-  property "Slice must sclice :)" do
-    check all l <- StreamData.list_of(StreamData.integer()),
-              i <- StreamData.integer(0..(length(l) - 1)),
-              k <- StreamData.integer((i + 1)..(length(l) - 1)) do
-      assert p118_slice(l, i + 1, k + 1) == Enum.slice(l, i, k - i + 1)
-    end
-  end
-"""
+
   test "p118_slice(list, i, k)" do
     assert p118_slice('abcdefghik', 3, 7) == [?c, ?d, ?e, ?f, ?g]
   end
@@ -227,37 +216,15 @@ defmodule NinetyNineProblemsTest do
     assert p119_rotate_n('abcdefgh', 3) == [?d, ?e, ?f, ?g, ?h, ?a, ?b, ?c]
     assert p119_rotate_n('abcdefgh', -2) == [?g, ?h, ?a, ?b, ?c, ?d, ?e, ?f]
   end
-"""
-  property "Remove must remove :)" do
-    check all l <- StreamData.list_of(StreamData.integer()),
-              k <- StreamData.integer(0..(length(l) - 1)) do
-      assert p120_remove_at(l, k + 1) |> elem(1) == List.delete_at(l, k)
-    end
-  end
-"""
+
   test "p120_remove_at(list, k)" do
     assert p120_remove_at('abcd', 2) == {?b, [?a, ?c, ?d]}
   end
-"""
-  property "Insert must insert :)" do
-    check all l <- StreamData.list_of(StreamData.integer()),
-              k <- StreamData.integer(0..(length(l) - 1)),
-              e <- StreamData.integer() do
-      assert p121_insert_at(l, k + 1, e) == List.insert_at(l, k, e)
-    end
-  end
-"""
+
   test "p121_insert_at(list, k, e)" do
     assert p121_insert_at('abcd', 2, ?z) == [?a, ?z, ?b, ?c, ?d]
   end
-"""
-  property "Range is a range :)" do
-    check all from <- StreamData.integer(),
-              to <- StreamData.integer() do
-      assert p122_range(from, to) == Enum.to_list(from..to)
-    end
-  end
-"""
+
   test "p122_range(from, to)" do
     assert p122_range(4, 9) == [4, 5, 6, 7, 8, 9]
   end
